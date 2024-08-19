4f81888cc362d08bc36de9a7bb56172ec35851f2
Gerardo Miranda <gerardodev25@gmail.com>
2024-08-19 18:21:25 -0400

    chore(dev): :construction: adding express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('hello world');
+import { envs } from './config/envs';
+import { Server } from './presentation/server';
+
+(async () => {
+  main();
+})();
+
+async function main() {
+  const server = new Server({ port: envs.PORT });
+
+  server.start();
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,33 @@
+import express, { Router } from 'express';
+
+interface Options {
+  port: number;
+  // routes: Router;
+  public_path?: string;
+}
+
+export class Server {
+  public readonly app = express();
+  private serverListener?: any;
+  private readonly port: number;
+
+  private readonly publicPath: string;
+  // private readonly routes: Router;
+
+  constructor(options: Options) {
+    // const { port, routes, public_path = 'public' } = options;
+    const { port, public_path = 'public' } = options;
+    this.port = port;
+    this.publicPath = public_path;
+    // this.routes = routes;
+  }
+
+  async start() {
+    this.serverListener = this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+  close() {
+    this.serverListener?.close();
+  }
+}
