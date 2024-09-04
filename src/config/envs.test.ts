c1c23785ca2f105a25f6bcbf5d579f613f112a5e
Gerardo Miranda <gerardodev25@gmail.com>
2024-09-03 21:34:49 -0400

    test(test): :white_check_mark: envs tested

---
+++ src/config/envs.test.ts
@@ -0,0 +1,28 @@
+import { envs } from './envs';
+
+describe('envs.tets.ts', () => {
+  test('should return options', () => {
+    expect(envs).toEqual({
+      PORT: 3000,
+      POSTGRES_USER: process.env.POSTGRES_USER,
+      POSTGRES_DB: process.env.POSTGRES_DB,
+      POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+      DB_PORT: 5430,
+      NODE_ENV: process.env.NODE_ENV,
+      DATABASE_URL: process.env.DATABASE_URL,
+    });
+  });
+
+  test('should return error if not found env', async () => {
+    jest.resetModules();
+    process.env.PORT = 'ABC';
+
+    try {
+      await import('./envs');
+
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(`${error}`).toContain('"PORT" should be a valid integer');
+    }
+  });
+});
