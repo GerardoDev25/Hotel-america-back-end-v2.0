56f87a65b4792512fff4a46078b4f33757a018aa
Gerardo Miranda <gerardodev25@gmail.com>
2024-09-06 18:58:59 -0400

    test(test): :white_check_mark: logger.service tested

---
+++ src/presentation/services/logger.service.test.ts
@@ -0,0 +1,30 @@
+import winston from 'winston';
+import { LoggerService } from './logger.service';
+
+describe('logger.service.ts', () => {
+  const service = 'test-service';
+  const localLogger = {
+    log: jest.fn(),
+    error: jest.fn(),
+  } as unknown as winston.Logger;
+
+  const logger = new LoggerService(service, localLogger);
+
+  test('should call log function', () => {
+    const message = 'test in log logger';
+    logger.log(message);
+
+    expect(localLogger.log).toHaveBeenCalled();
+    expect(localLogger.log).toHaveBeenCalledTimes(1);
+    expect(localLogger.log).toHaveBeenCalledWith('info', { message, service });
+  });
+
+  test('should call error function', () => {
+    const message = 'test in log logger';
+    logger.error(message);
+
+    expect(localLogger.error).toHaveBeenCalled();
+    expect(localLogger.error).toHaveBeenCalledTimes(1);
+    expect(localLogger.error).toHaveBeenCalledWith('error', { message, service });
+  });
+});
