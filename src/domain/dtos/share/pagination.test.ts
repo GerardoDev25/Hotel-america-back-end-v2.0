b22e0217d9e23f04621932195a47d4efd8890a58
Gerardo Miranda <gerardodev25@gmail.com>
2024-09-11 19:43:23 -0400

    refactor(test): :white_check_mark: pagination suit test refactor

--- src/domain/dtos/share/pagination.test.ts
+++ src/domain/dtos/share/pagination.test.ts
@@ -15,30 +15,25 @@ describe('pagination.ts', () => {
     expect(error).toBeUndefined();
   });
 
+  test('should get a error message if page or limit are not numbers', () => {
+    const [error1, resultPage] = PaginationDto.create('page' as any, limit);
+    const [error2, resultLimit] = PaginationDto.create(page, 'limit' as any);
 
-  test('should get a error message if page or limit are not numbers', () => { 
-    
-    const resultPage = PaginationDto.create('page' as unknown as number, limit);
-    const resultLimit = PaginationDto.create(page, 'limit' as unknown as number);
+    expect(error1).toBe('Page and limit must be a number');
+    expect(error2).toBe('Page and limit must be a number');
 
-    expect(resultPage[0]).toBe('Page and limit must be a number');
-    expect(resultLimit[0]).toBe('Page and limit must be a number');
+    expect(resultPage).toBeUndefined();
+    expect(resultLimit).toBeUndefined();
+  });
 
-    expect(resultPage[1]).toBeUndefined();
-    expect(resultLimit[1]).toBeUndefined();
-    
-   })
+  test('should get a error message if page or limit are negatives numbers', () => {
+    const [error1, resultPage] = PaginationDto.create(-2, limit);
+    const [error2, resultLimit] = PaginationDto.create(page, -2);
 
-  test('should get a error message if page or limit are negatives numbers', () => { 
-    
-    const resultPage = PaginationDto.create(-2, limit);
-    const resultLimit = PaginationDto.create(page, -2);
+    expect(error1).toBe('Page must be greaten than 0');
+    expect(error2).toBe('Limit must be greaten than 0');
 
-    expect(resultPage[0]).toBe('Page must be greaten than 0');
-    expect(resultLimit[0]).toBe('Limit must be greaten than 0');
-
-    expect(resultPage[1]).toBeUndefined();
-    expect(resultLimit[1]).toBeUndefined();
-    
-   })
+    expect(resultPage).toBeUndefined();
+    expect(resultLimit).toBeUndefined();
+  });
 });
