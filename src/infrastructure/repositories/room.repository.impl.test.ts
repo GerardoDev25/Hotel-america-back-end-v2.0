785a053c53420ab254fb12259d9bda7d2f5537b6
Gerardo Miranda <gerardodev25@gmail.com>
2024-09-10 21:24:18 -0400

    refactor(test): :white_check_mark: refactor room.repository.impl

--- src/infrastructure/repositories/room.repository.impl.test.ts
+++ src/infrastructure/repositories/room.repository.impl.test.ts
@@ -1,3 +1,4 @@
+import { Uuid } from '../../adapters';
 import { RoomDatasource } from '../../domain/datasources';
 import { CreateRoomDto, UpdateRoomDto } from '../../domain/dtos/room';
 import { RoomEntity } from '../../domain/entities';
@@ -7,53 +8,14 @@ import { RoomRepositoryImpl } from './room.repository.impl';
 describe('room.repository.impl.ts', () => {
   const page = 2;
   const limit = 10;
-  const isAvailable = true;
 
-  const createRoom: CreateRoomDto = {
-    roomType: RoomTypesList.SUIT,
-    roomNumber: 1,
-    betsNumber: 1,
-    isAvailable: false,
-  };
-
-  const updateRoom: UpdateRoomDto = {
-    id: 'abv',
-    roomType: RoomTypesList.NORMAL,
-    roomNumber: 2,
-    betsNumber: 2,
-    isAvailable: true,
-  };
-
-  const mockRoom: RoomEntity = new RoomEntity({
-    id: 'abc',
-    roomType: RoomTypesList.NORMAL,
-    roomNumber: 12,
-    betsNumber: 12,
-    isAvailable: true,
-  });
-  const mockRoom2: RoomEntity = new RoomEntity({
-    id: 'abc',
-    roomType: RoomTypesList.NORMAL,
-    roomNumber: 12,
-    betsNumber: 12,
-    isAvailable: true,
-  });
-
-  const getAllReturnValue = {
-    rooms: [mockRoom, mockRoom2],
-    limit,
-    next: '',
-    page,
-    prev: '',
-    total: 0,
-  };
   const mockDatasource: RoomDatasource = {
-    getAll: jest.fn().mockResolvedValue(getAllReturnValue),
-    getAllAvailable: jest.fn().mockResolvedValue(getAllReturnValue),
-    getById: jest.fn().mockResolvedValue(mockRoom),
-    create: jest.fn().mockResolvedValue(mockRoom2),
-    update: jest.fn().mockResolvedValue(mockRoom2),
-    delete: jest.fn().mockResolvedValue(mockRoom2),
+    getAll: jest.fn(),
+    getAllAvailable: jest.fn(),
+    getById: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
   };
   const repository = new RoomRepositoryImpl(mockDatasource);
 
@@ -62,50 +24,69 @@ describe('room.repository.impl.ts', () => {
   });
 
   test('should call getAll', async () => {
-    const rooms = await repository.getAll(page, limit);
+    await repository.getAll(page, limit);
 
     expect(mockDatasource.getAll).toHaveBeenCalled();
     expect(mockDatasource.getAll).toHaveBeenCalledWith(page, limit);
-
-    expect(rooms).toEqual(getAllReturnValue);
   });
 
   test('should call getAllAvailable', async () => {
-    const roomsAvailable = await repository.getAll(page, limit, isAvailable);
+    const isAvailable = true;
 
-    expect(mockDatasource.getAllAvailable).toHaveBeenCalled();
+    await repository.getAll(page, limit, isAvailable);
+
+    expect(mockDatasource.getAllAvailable).toHaveBeenCalledTimes(1);
     expect(mockDatasource.getAllAvailable).toHaveBeenCalledWith(
       page,
       limit,
       isAvailable
     );
-    expect(roomsAvailable).toEqual(getAllReturnValue);
   });
 
   test('should call getById', async () => {
-    const room = await repository.getById('some-id');
-    expect(mockDatasource.getById).toHaveBeenCalledWith(expect.any(String));
-    expect(room).toEqual(mockRoom);
+    const id = Uuid.v4();
+
+    await repository.getById(id);
+
+    expect(mockDatasource.getById).toHaveBeenCalledTimes(1);
+    expect(mockDatasource.getById).toHaveBeenCalledWith(id);
   });
 
   test('should call create with parameter', async () => {
-    const room = await repository.create(createRoom);
+    const createRoom: CreateRoomDto = {
+      roomType: RoomTypesList.SUIT,
+      roomNumber: 1,
+      betsNumber: 1,
+      isAvailable: false,
+    };
+
+    await repository.create(createRoom);
 
     expect(mockDatasource.create).toHaveBeenCalledTimes(1);
     expect(mockDatasource.create).toHaveBeenCalledWith(createRoom);
-    expect(room).toEqual(mockRoom2);
   });
 
   test('should call update with parameter', async () => {
-    const room = await repository.update(updateRoom);
+    const updateRoom: UpdateRoomDto = {
+      id: 'abv',
+      roomType: RoomTypesList.NORMAL,
+      roomNumber: 2,
+      betsNumber: 2,
+      isAvailable: true,
+    };
+
+    await repository.update(updateRoom);
+
     expect(mockDatasource.update).toHaveBeenCalledTimes(1);
     expect(mockDatasource.update).toHaveBeenCalledWith(updateRoom);
-    expect(room).toEqual(mockRoom2);
   });
 
   test('should call delete', async () => {
-    const room = await repository.delete('some-id');
-    expect(mockDatasource.delete).toHaveBeenCalledWith(expect.any(String));
-    expect(room).toEqual(mockRoom2);
+    const id = Uuid.v4();
+
+    await repository.delete(id);
+
+    expect(mockDatasource.delete).toHaveBeenCalledTimes(1);
+    expect(mockDatasource.delete).toHaveBeenCalledWith(id);
   });
 });
