e69b8ea15fac2374391154d1d7436da49c75971f
Gerardo Miranda <gerardodev25@gmail.com>
2024-09-26 21:28:44 -0400

    chore(dev): :construction: add guest repository implementation

---
+++ src/infrastructure/repositories/guest.repository.impl.ts
@@ -0,0 +1,41 @@
+import { CreateGuestDto, UpdateGuestDto } from '@domain/dtos/guest';
+import { GuestDatasource } from '@domain/datasources/guest.datasource';
+import { GuestEntity } from '@domain/entities';
+import { GuestRepository } from '@domain/repositories';
+import { IGuest, GuestPagination } from '@domain/interfaces';
+
+export class GuestRepositoryImpl extends GuestRepository {
+  constructor(private readonly guestDatasource: GuestDatasource) {
+    super();
+  }
+
+  getById(id: string): Promise<{ ok: boolean; guest: GuestEntity }> {
+    return this.guestDatasource.getById(id);
+  }
+
+  getByParam(
+    searchParam: Partial<Pick<IGuest, keyof IGuest>>
+  ): Promise<{ ok: boolean; guest: GuestEntity | null }> {
+    return this.guestDatasource.getByParam(searchParam);
+  }
+
+  getAll(page: number, limit: number): Promise<GuestPagination> {
+    return this.guestDatasource.getAll(page, limit);
+  }
+
+  create(
+    createGuestDto: CreateGuestDto
+  ): Promise<{ ok: boolean; guest: GuestEntity }> {
+    return this.guestDatasource.create(createGuestDto);
+  }
+
+  update(
+    updateGuestDto: UpdateGuestDto
+  ): Promise<{ ok: boolean; message: string }> {
+    return this.guestDatasource.update(updateGuestDto);
+  }
+
+  delete(id: string): Promise<{ ok: boolean; message: string }> {
+    return this.guestDatasource.delete(id);
+  }
+}
